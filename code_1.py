9296304d5e590b2f1e9fd889296597865a502e9d
ccruz <culturalcruzmarinc@gmail.com>
2024-01-02 16:04:27 +0100

    Dispersion energy (1/2)

--- code_1.py
+++ code_1.py
@@ -9,29 +9,57 @@ import time
 def convert_fortran_to_python(num_string):
     return float(num_string.replace('d','e'))
 
+##################################### PARAMETERS FROM FILE #####################################
 def parameters_assigment(all_params):
+    alpha_dict = {}
+    zeff_dict = {}
+    q_dict = {}
+    r_dict = {}
     
     #enumerate gives us the chance of getting the index of each element in the list
     for i, line in enumerate(all_params): 
         if 'kf' in line:
-            print(line)
-            
-            #When there is a match, we look for the following line for storing the values
+            #When there is a match, we look for the following line for storing the values in dictionaries using the function zip
             kf = float(all_params[i+1])
-            print(kf)
             
-        elif 'alpha' in line:
-            print(line)
+        elif 'alpha' in line: 
             alpha = all_params[i+1].split()
+            for key,value in zip(element_list,alpha):
+                alpha_dict [key] = value
             
-        elif 'Zeff' in line:
-            print(line)
+        elif 'Zeff' in line:  
             zeff = all_params[i+1].split()
-            
-    return(kf,alpha,zeff)
+            for key,value in zip(element_list,zeff):
+                zeff_dict [key] = value
+                
+        elif 'bohr to Angstrom' in line:
+            distance_conversion = float(convert_fortran_to_python(all_params[i+1]))
+        
+        elif 'QA values' in line:
+            q = all_params[i+1].split()
+            for key, value in zip(element_list,q):
+                q_dict [key] = value
+        
+        elif 'Overall parameters' in line:
+            a1 = (convert_fortran_to_python((all_params[i+1]).split()[0]))
+            a2 = (convert_fortran_to_python((all_params[i+1]).split()[1]))
+            s6 = (convert_fortran_to_python((all_params[i+1]).split()[2]))
+            s8 = (convert_fortran_to_python((all_params[i+1]).split()[3]))
+            kcn = (convert_fortran_to_python((all_params[i+1]).split()[4]))
+            kl = (convert_fortran_to_python((all_params[i+1]).split()[5]))
+        
+        elif 'covalent radii' in line:
+            radii = all_params[i+1].split()
+            for key, value in zip(element_list,radii):
+                r_dict [key] = convert_fortran_to_python(value) * (4/3) * (1/distance_conversion)
+    
 
+    return(distance_conversion,kf,alpha_dict,zeff_dict,q_dict,a1,a2,s6,s8,kcn,kl,r_dict)
+###############################################################################################################
 
-def repulsion_energy(num_atoms):
+##################################### REPULSION TERM #####################################
+
+def repulsion_contribution():
     
     #Initialization of the repulsion energy term
     repulsion_energy = 0
@@ -39,13 +67,67 @@ def repulsion_energy(num_atoms):
         for b in range(a+1, num_atoms):
             dist_ab = 0
             for i in range(3):
-                dist_ab += (coordinates[a,i]-coordinates[b,i])**2
+                dist_ab += (coordinates_bohr[a,i]-coordinates_bohr[b,i])**2
                 
             dist_ab = np.sqrt(dist_ab)
             print("Distance between",atom_list[a],"and",atom_list[b],"is:",dist_ab)    
-            #repulsion_energy += (z_dict.get(element_list[a]) * z_dict.get(element_list[b])/dist_ab) * np.exp(-np.sqrt() * dist_ab ** kf) 
+            repulsion_energy += (convert_fortran_to_python(zeff_dict.get(atom_list[a])) * convert_fortran_to_python( zeff_dict.get(atom_list[b])))/(dist_ab) * np.exp(-np.sqrt(convert_fortran_to_python(alpha_dict.get(atom_list[a]))*convert_fortran_to_python(alpha_dict.get(atom_list[b]))) * dist_ab ** kf) 
     
+    print("Repulsion energy is:",repulsion_energy,'Hartree')
     return(repulsion_energy)
+###############################################################################################################
+
+##################################### DISPERSION TERM #####################################
+
+def switcher(n):
+    if n == 6:
+        value = s6
+    elif n == 8:
+        value = s8
+    return(value)
+
+def cn(a,b,n,coord_number):
+
+    if n == 6:
+        value = 1
+    elif n == 8:
+        c6 =   1     
+        value = 3 * c6 * np.sqrt(convert_fortran_to_python(q_dict.get(atom_list[a])) * convert_fortran_to_python(q_dict.get(atom_list[b])))
+    
+    damping_constant = (9 * convert_fortran_to_python(q_dict.get(atom_list[a])) * convert_fortran_to_python(q_dict.get(atom_list[b]))) ** (1/4)
+    
+    return(value,damping_constant)
+
+def damping_func(dist_ab,n,damping_constant):
+    
+    damp = (dist_ab**n)/((dist_ab**n) + (a1*damping_constant + a2) ** n)
+    return(damp)
+
+def coord_calc(a,num_atoms):
+    coord_number = 0
+    for b in range(num_atoms):
+        if b != a:
+            coord_number += (1 + np.exp(-kcn * r_dict.get(atom_list[a]) + r_dict.get(atom_list[b])) ** -1) 
+
+def dispersion_contribution():
+    
+    #Initialization of the dispersion energy term
+    dispersion_energy = 0
+    for a in range(num_atoms):
+        coord_number = coord_calc(a,num_atoms)
+        for b in range (a+1, num_atoms):
+            dist_ab = 0
+            for i in range(3):
+                dist_ab += (coordinates_bohr[a,i]-coordinates_bohr[b,i])**2
+                
+            dist_ab = np.sqrt(dist_ab)
+            for n in [6, 8]:
+                cn_n,damping_constant= cn(a,b,n,coord_number)
+                dispersion_energy += ((-switcher(n) * cn_n)/(dist_ab**n)) * damping_func(dist_ab,n,damping_constant)
+            
+    
+    return(dispersion_energy)
+###############################################################################################################
 
 if __name__ == '__main__': 
     st = time.time()
@@ -63,7 +145,8 @@ if __name__ == '__main__':
     # Files reading
     with open(parameters,'r') as file:
        all_params = file.read().split('\n')
-       print(all_params)
+       
+          
        
     with open(molecule,'r') as file:
         num_atoms = int(file.readline().strip())
@@ -74,7 +157,7 @@ if __name__ == '__main__':
            data = file.readline().strip()
            atom_list.append(data.split()[0])
            
-           # After the first two lines, the atoms
+           # After the first two lines, the atom's coordinates in Angstroms
            for i in range(3):
                coordinates[n][i] = data.split()[i+1]
            
@@ -83,18 +166,16 @@ if __name__ == '__main__':
     for i in range(num_atoms):
         print("Atom of the system:",atom_list[i],"with coordinates:",coordinates[i,:])
     
-    parameters_assigment(all_params)
-    repulsion_energy(num_atoms)
+    element_list = ['H','C','N','O']
+    
+    distance_conversion,kf,alpha_dict,zeff_dict,q_dict,a1,a2,s6,s8,kcn,kl,r_dict= parameters_assigment(all_params)
+    coordinates_bohr = coordinates * 1 / distance_conversion
+    
+    repulsion_contribution()
+    dispersion_contribution()
+    
     
     
     et = time.time()
     
-    print("Execution time in seconds {:.2f}".format(et-st))
-        
-    
-
-    
-        
-    
-
-    
+    print("Execution time in seconds {:.2f}".format(et-st))  
