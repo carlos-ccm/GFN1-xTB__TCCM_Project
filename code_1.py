a478fa72308b4c7e7ce5e18beb1c0935587b32b7
ccruz <culturalcruzmarinc@gmail.com>
2024-02-02 08:33:27 +0100

    First cycle done

--- code_1.py
+++ code_1.py
@@ -202,10 +202,9 @@ def basis_set_select(all_params):
     
     match = False
     basis_functions_dict = {}
-
-    
     #This part is for extracting specifically the part of parameters.dat that contains the information about the basis set
     basis_sets_info = []
+
     for line in all_params:
         if '(c) d contraction' in line:
             match = True
@@ -242,7 +241,7 @@ def basis_set_select(all_params):
             'Contraction': basis_function_contraction,
             'Exponent': basis_function_zeta}
 
-    basis_functions = {}
+    basis_functions= {}
     n = 0
     for i,at in enumerate(atom_list):
         for j in range(basis_functions_per_atom_type.get(at)):
@@ -252,7 +251,6 @@ def basis_set_select(all_params):
             else:
                 function_type = 1
             shell = list(basis_functions_dict[str(dictionary_atom_types.get(at))].items())[function_type][0]
-
             basis_functions[n] = {
             'Atom':at,
             'Atom index':i,
@@ -261,8 +259,16 @@ def basis_set_select(all_params):
             'Exponent':basis_functions_dict[str(dictionary_atom_types.get(at))][str(shell)]['Exponent']            
             } 
             n += 1
-
-    return (basis_functions_dict,basis_functions)
+    n = 0
+    shell_dict = {}
+    for i,at in enumerate(atom_list):
+        for j in range(2):
+            shell_dict[n] = {
+                "Index":i,
+                "Element": at,
+                "Shell type":list(basis_functions_dict[str(dictionary_atom_types.get(at))].items())[j][0]}
+            n += 1
+    return (basis_functions_dict,basis_functions,shell_dict)
 
 def overlap_eval():
     overlap_matrix = np.zeros((num_basis_funcs, num_basis_funcs))
@@ -367,7 +373,7 @@ def inverse_square_root_overlap(overlap_matrix):
     return(S_inv_sqrt)
 
 def scaling_function(μ,v,basis_functions):
-    if  (basis_functions[μ]['Atom'] == 'H' and basis_functions[v]['Atom'] == 'H'):
+    if  (basis_functions[μ]['Atom'] == 'H' and basis_functions[μ]['Function type'][0] == '1' and basis_functions[v]['Atom'] == 'H' and basis_functions[v]['Function type'][0] == '1'):
         scaling_parameter = khh
     elif (basis_functions[μ]['Atom'] == 'N' and basis_functions[v]['Atom'] == 'H') or (basis_functions[μ]['Atom'] == 'H' and basis_functions[v]['Atom'] == 'N'):
         scaling_parameter = khn
@@ -375,16 +381,17 @@ def scaling_function(μ,v,basis_functions):
         scaling_parameter = 1 
            
     return (scaling_parameter)
-
-def electronic_energy(basis_functions):
+        
+def electronic_energy(basis_functions,shell_dict):
     ##Calculation of the electronic term is shown below ##    
-        #First the mu|H0|nu term, from the 0th order Hamiltonian
+    #First the mu|H0|nu term, from the 0th order Hamiltonian
     H0 = np.zeros((num_basis_funcs,num_basis_funcs))
     for μ in range(num_basis_funcs):
-       for v in range(num_basis_funcs):
+        for v in range(num_basis_funcs):
+            
             h_l_a = hamiltonian_parameters_dict[str(dictionary_atom_types.get(basis_functions[μ]['Atom']))][basis_functions[μ]['Function type'][0]]['HAl'] * (1 + kcn_dict[basis_functions[μ]['Function type'][0]] * coord_calc(basis_functions[μ]['Atom index'], num_atoms)) #effective atomic energy level h_l_a 
             h_l_b = hamiltonian_parameters_dict[str(dictionary_atom_types.get(basis_functions[v]['Atom']))][basis_functions[v]['Function type'][0]]['HAl'] * (1 + kcn_dict[basis_functions[v]['Function type'][0]] * coord_calc(basis_functions[v]['Atom index'], num_atoms)) #effective atomic energy level h_l_b
-            if μ == v:
+            if μ == v: #(incluir si el atom index es el mismo aqui)
                 H0[μ,v] = hamiltonian_parameters_dict[str(dictionary_atom_types.get(basis_functions[μ]['Atom']))][basis_functions[μ]['Function type'][0]]['HAl'] * (1 + kcn_dict[basis_functions[μ]['Function type'][0]] * coord_calc(basis_functions[μ]['Atom index'], num_atoms)) #effective atomic energy level h_l_a 
             else:
                 scaling_parameter = scaling_function(μ,v,basis_functions)
@@ -394,20 +401,67 @@ def electronic_energy(basis_functions):
                 for i in range(3):
                     dist_ab += (coordinates_bohr[a,i]-coordinates_bohr[b,i])**2                            
                 dist_ab = np.sqrt(dist_ab)
-                R_AB = r_dict_2.get(basis_functions[μ]['Atom']) + r_dict_2.get(basis_functions[v]['Atom'])
-                        
+                R_AB = r_dict_2.get(basis_functions[μ]['Atom']) + r_dict_2.get(basis_functions[v]['Atom'])  
                 pi_rab_ll = (1 + hamiltonian_parameters_dict[str(dictionary_atom_types.get(basis_functions[μ]['Atom']))][basis_functions[μ]['Function type'][0]]['kpoly'] * ((dist_ab/R_AB)**0.5)) * (1 + hamiltonian_parameters_dict[str(dictionary_atom_types.get(basis_functions[v]['Atom']))][basis_functions[v]['Function type'][0]]['kpoly'] * ((dist_ab/R_AB)**0.5))
                 if basis_functions[μ]['Function type'][0] == '2' or basis_functions[v]['Function type'][0] == '2':
-                    variation_electronegativity_term = 1
+                    variation_electronegativity_term = 1 
                 else:
                     variation_electronegativity_term = 1 + kEN * (electronegativities_dict.get(basis_functions[μ]['Atom']) - electronegativities_dict.get(basis_functions[v]['Atom']))**2
-                
-                if float(basis_functions[μ]['Function type'][0]) <= float(basis_functions[v]['Function type'][0]):
+                    
+                if int(basis_functions[μ]['Function type'][0]) <= int(basis_functions[v]['Function type'][0]):
                     H0[μ,v]  = scaling_parameter * kll_dict[str(basis_functions[μ]['Function type'][0])][str(basis_functions[v]['Function type'][0])] * 0.5 * (h_l_a + h_l_b) * overlap_matrix[μ,v] * variation_electronegativity_term * pi_rab_ll
+                    
                 else:
-                    H0[μ,v]  = scaling_parameter * kll_dict[str(basis_functions[v]['Function type'][0])][str(basis_functions[μ]['Function type'][0])] * 0.5 * (h_l_a + h_l_b) * overlap_matrix[μ,v] * variation_electronegativity_term * pi_rab_ll                
+                    H0[μ,v]  = scaling_parameter * kll_dict[str(basis_functions[v]['Function type'][0])][str(basis_functions[μ]['Function type'][0])] * 0.5 * (h_l_a + h_l_b) * overlap_matrix[μ,v] * variation_electronegativity_term * pi_rab_ll                   
     print("\n0th Hamiltonian\n")
     print(H0)
+    kg = 2
+    gamma = np.zeros((num_shells,num_shells))
+    #now we calculate the coulomb kernel matrix
+    for A in range(num_shells):
+       for B in range(num_shells): 
+            dist_ab = 0
+            for i in range(3):
+                dist_ab += (coordinates_bohr[shell_dict[A]['Index'],i]-coordinates_bohr[shell_dict[B]['Index'],i]) ** 2                                
+            dist_ab = np.sqrt(dist_ab)
+            
+            η_A = hamiltonian_parameters_dict[str(dictionary_atom_types.get(shell_dict[A]['Element']))][shell_dict[A]['Shell type']]['eta_Al']
+            η_B = hamiltonian_parameters_dict[str(dictionary_atom_types.get(shell_dict[B]['Element']))][shell_dict[B]['Shell type']]['eta_Al']
+            η_AB = 1 / (0.5 * (1/η_A + 1/η_B))
+            gamma[A,B] = (1 / ((dist_ab)**kg + (η_AB)**(-kg))) ** (1/kg)
+    
+    #now we start the SCF process
+    fock_matrix = H0
+    P_matrix = np.array((num_basis_funcs,num_basis_funcs))
+    error = 100000000
+    threshold = 1
+    while error > threshold:
+        
+        fock_matrix_prime = np.transpose(S_inv_sqrt) @ fock_matrix @ S_inv_sqrt
+        eigenvalues, C_prime = np.linalg.eig(fock_matrix_prime)
+        C = S_inv_sqrt @ C_prime
+        print("\nEigenvalues")
+        print(eigenvalues)
+        
+        print("\nH0 prime")
+        print(fock_matrix_prime)
+        
+        sorted_indices = np.argsort (eigenvalues)
+        for i in range(len(sorted_indices)):
+            print(i,sorted_indices[i])
+            C[i,:] = C[:,sorted_indices[i]]
+                
+        print("\nCoefficient matrix ordered")
+        print(C)
+        
+        for μ in range(num_basis_funcs):
+            for v in range(num_basis_funcs):
+                for k in range(1,int(num_shells/2)):
+                    P_matrix[μ,v] += C[μ,k] @ C[v,k]
+        P_matrix = 2 * P_matrix
+        print(P_matrix)
+        error = 0 
+            
     return(H0)    
         
 if __name__ == '__main__': 
@@ -423,7 +477,7 @@ if __name__ == '__main__':
     parameters = args.p
     atom_list = []
 
-    # Files reading
+    #Files reading
     with open(parameters,'r') as file:
        all_params = file.read().split('\n')
        
@@ -458,8 +512,9 @@ if __name__ == '__main__':
         system_elec += electrons_per_atom_type.get(atom_list[i])
         num_basis_funcs += basis_functions_per_atom_type.get(atom_list[i])
         
+    num_shells = num_atoms * 2  
     print('Number of electrons and occupied orbitals:',system_elec,int(system_elec/2))
-    print('Total number of shells and basis functions:',num_atoms*2,num_basis_funcs)
+    print('Total number of shells and basis functions:',num_shells,num_basis_funcs)
     
     distance_conversion,energy_conversion,kf,alpha_dict,zeff_dict,q_dict,a1,a2,s6,s8,kcn,kl,r_dict,ref_coord_num_dict,na_nb_dict,khh,khn,kll_dict,kcn_dict,hamiltonian_parameters_dict,kEN,electronegativities_dict,r_dict_2= parameters_assigment(all_params)
     coordinates_bohr = coordinates * 1 / distance_conversion
@@ -471,15 +526,12 @@ if __name__ == '__main__':
     repulsion_contribution()
     dispersion_contribution()
     
-    
     #Now we calculate the components of the electronic energy
-    basis_functions_dict, basis_functions = basis_set_select(all_params)
+    basis_functions_dict, basis_functions, shell_dict = basis_set_select(all_params)
     overlap_matrix = overlap_eval()
     S_inv_sqrt = inverse_square_root_overlap(overlap_matrix)
     
-    H0 = electronic_energy(basis_functions)
-    
-    
-    et = time.time()
-    
+    H0 = electronic_energy(basis_functions,shell_dict)
+   
+    et = time.time()   
     print("Execution time in seconds {:.2f}".format(et-st))  
